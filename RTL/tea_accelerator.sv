7583f17baaf5465590cd0de12c787c776485d03d
Mitchell Sharum <msharum@college.harvard.edu>
2025-03-04 00:15:21 -0500

    Initial implementation of hardware TEA, still need to hammer out processing details due to register dependencies between v0 and v1.

--- RTL/tea_accelerator.sv
+++ RTL/tea_accelerator.sv
@@ -16,14 +16,15 @@ module tea_accel(
     output logic o_axis_valid_m,            // asserted to inform downstream circuit that data is ready
     input logic i_axis_ready_m,             // asserted by downstream circuit to rquest encrypted data
     output logic [63:0] o_axis_data_m       // the encrypted output
-)
+);
 
     // register declarations
     logic [1:0] state, next_state;
     logic [4:0] round_counter; 
     logic [31:0] v0, v1;
     logic [31:0] k0, k1, k2, k3;
-    logic [63:0] output_data_reg = 0;
+    logic [31:0] sum = 0;
+    logic [63:0] output_data = 0;
 
     // state enumeration
     localparam IDLE = 2'b00;
@@ -33,46 +34,122 @@ module tea_accel(
 
     // delta constant apparently adds non-linearity
     localparam DELTA = 32'h9E3779B9;
-
-    // split the key and input into registers for operating rounds
-    assign v0 = i_axis_data_s[31:0];
-    assign v1 = i_axis_data_s[63:32];
-    assign k0 = i_key[31:0];
-    assign k1 = i_key[63:32];
-    assign k2 = i_key[95:64];
-    assign k3 = i_key[127:96];
     
     // combinatorial block for state transitions
     always @(*) begin
         case (state)
+            
+            // when module ready to accept a data word and the upstream module is ready to provide, then move into LOADING state 
             IDLE: 
+                if (i_axis_valid_s && o_axis_ready_s) begin
+                    next_state = LOADING;
+                end
+
+            // load the inoput data into corresponding registers
             LOADING:
+                next_state = PROCESSING;
+            
+            // process 32 rounds before output
             PROCESSING:
+                if (round_counter >= 31) begin
+                    next_state = DONE;
+                end
+            
+            // if the downstream module is ready to receive, output encrypted data and return to IDLE       
             DONE:
+                if (o_axis_valid_m && i_axis_ready_m) begin
+                    next_state = IDLE;
+                end
+            
+            // default case, remain in the same state
+            default: 
+                next_state = state;
         endcase
     end
 
     // sequential block for computing rounds
     always @(posedge i_clk) begin
         
+        // transition to the next state
+        state <= next_state;
+
         // reset handling
         if (i_rst) begin
+            
+            // reset state and outputs
             state <= IDLE;
+            sum <= 0;
             round_counter <= 0;
-            output_data_reg <= 0;
             o_axis_ready_s <= 0;
             o_axis_valid_m <= 0;
             o_axis_data_m <= 0;
+            
+            // zero operator registers
+            v0 <= 0;
+            v1 <= 0;
+            k0 <= 0;
+            k1 <= 0;
+            k2 <= 0;
+            k3 <= 0;
+
+        // IDLE handling
+        end else if (state == IDLE) begin
+        
+            // reset the data, sum, counter, and output registers for next data block
+            sum <= 0;
+            round_counter <= 0;
+            output_data <= 0;
+            
+            // zero operator registers
+            v0 <= 0;
+            v1 <= 0;
+            k0 <= 0;
+            k1 <= 0;
+            k2 <= 0;
+            k3 <= 0;
+
+            // assert that we are ready for another input block
+            o_axis_ready_s <= 1'b1;
         
         // LOADING handling
         end else if (state == LOADING) begin
+            
+            // load the key and input data into registers for operating rounds
+            v0 <= i_axis_data_s[31:0];
+            v1 <= i_axis_data_s[63:32];
+            k0 <= i_key[31:0];
+            k1 <= i_key[63:32];
+            k2 <= i_key[95:64];
+            k3 <= i_key[127:96];
+
+            // assert we are no longer ready to receive
+            o_axis_ready_s <= 1'b0;
         
         // PROCESSING handling
         end else if (state == PROCESSING) begin
+
+            // increent the sum by DELTA
+            sum <= sum + DELTA;
+
+            // process v0 and v1 rounds respectively
+            v0 <= v0 + (((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1));
+            v1 <= v1 + (((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3));
+
+            // increment the round counter by 1
+            round_counter <= round_counter + 1;
         
         // DONE handling
         end else if (state == DONE) begin
         
+            // output assignment - concatenate the v0 and v1
+            output_data <= {v0,v1};
+
+            // tell downstream module we are ready to transmit
+            o_axis_valid_m <= 1'b1;
         end
     end
+
+    // final output assignment
+    assign o_axis_data_m = output_data;
+
 endmodule
